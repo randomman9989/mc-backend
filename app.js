002a2fedb6cae0cff559aa0f66cab101f3d6bb3c
Nicholas Eldergill <Nicholas.Eldergill@city.ac.uk>
2025-08-12 10:22:11 +0100

    add cors to api

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const logger = require(`./logger`)
-
 const fruits = require("./routes/fruits")
+
+app.use(cors)
+app.use(express.json());
+app.use(logger)
 
 app.get('/', (req, res) => {
    res.send('Hello Fruity!');
 })
 
-app.use(express.json());
-
-app.use(logger)
 
 app.use('/fruits', fruits);
 
